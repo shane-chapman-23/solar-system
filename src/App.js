ab3fef945e7dad5a97b6b8b59d340e41ae2f4f55
Shane Chapman <shane@Shanes-MacBook-Pro.local>
2023-04-21 20:42:20 +1200

    added nav bar and function to show planetData when nav is clicked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { useState } from 'react';
 import './App.css';
+import planetData from './planetData.js';
 
 function App() {
+  const [selectedPlanet, setSelectedPlanet] = useState(null);
+
+  const handlePlanetClick = (planet) => {
+    setSelectedPlanet(planet);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Planets in our</h2>
         <h1>Solar System</h1>
       </header>
+      <div className="Main-body">
+        <nav className="Planet-list">
+          <ul>
+            {planetData.map((planet) => {
+              return <li key={planet.name} onClick={() => handlePlanetClick(planet)}>
+                {planet.name}
+              </li>
+            })}
+          </ul>
+        </nav>
+          {selectedPlanet && (
+            <div className="Planet-display">
+              <h2>{selectedPlanet.name}</h2>
+            </div>
+          )}
+      </div>
     </div>
   );
 }
---
+++ src/planetData.js
@@ -0,0 +1,49 @@
+const planetData = [
+    {
+        name: "Sun",
+        image: "Sun Image",
+        distFromSun: 0
+    },
+    {
+        name: "Mercury",
+        image: "Mercury Image",
+        distFromSun: 57910000
+    },
+    {
+        name: "Venus",
+        image: "Venus Image",
+        distFromSun: 108200000 
+    },
+    {
+        name: "Earth",
+        image: "Earth Image",
+        distFromSun: 149600000
+    },
+    {
+        name: "Mars",
+        image: "Mars Image",
+        distFromSun: 227940000
+    },
+    {
+        name: "Jupiter",
+        image: "Jupiter Image",
+        distFromSun: 778330000
+    },
+    {
+        name: "Saturn",
+        image: "Saturn Image",
+        distFromSun: 1424600000
+    },
+    {
+        name: "Uranus",
+        image: "Uranus Image",
+        distFromSun: 2873550000
+    },
+    {
+        name: "Neptune",
+        image: "Neptune Image",
+        distFromSun: 4501000000
+    },
+];
+
+export default planetData;
